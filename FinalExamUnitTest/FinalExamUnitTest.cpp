6a3c12d23a2134e9ba8e4cb3c287b15bd70d2199
Marios Kyriacou <kyriacou.m6@live.unic.ac.cy>
2025-05-27 09:28:00 +0300

    Implemented a unit test for the CountValue function.

--- FinalExamUnitTest/FinalExamUnitTest.cpp
+++ FinalExamUnitTest/FinalExamUnitTest.cpp
@@ -7,12 +7,22 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 namespace FinalExamUnitTest {
 	TEST_CLASS(FinalExamUnitTest) {
 		public:
-			TEST_METHOD(TestMethod1) {
+			TEST_METHOD(CountValueUnitTest) {
 				// Arrange
+				const double matrix[3][5] = {
+					{-1.5, 0.5, 0.0, -1.5, -0.5},
+					{-1.2, 0.2, 0.5, -0.5, 0.0},
+					{0.0, -1.5, -1.5, 0.5, -0.5}
+				};
+				int output = 0;
 
 				// Act
+				for (int i = 0; i < 3; i++) {
+					output += CountValue(matrix[i], 5, 1.5);
+				}
 
 				// Assert
+				Assert::AreEqual(0, output, L"CountValue should return 0 for no occurrences of 1.5");
 			}
 	};
 }
