eedfc7508388028c9177def6948ac9ccb04a1b83
Marios Kyriacou <kyriacou.m6@live.unic.ac.cy>
2025-05-27 08:32:57 +0300

    Add project files.

---
+++ FinalExam.cpp
@@ -0,0 +1,17 @@
+/**
+ * \file FinalExam.cpp
+ * \brief
+ * \details
+ * \author Marios Kyriacou (U244N0037)
+ * \version 0.1
+ * \date 2025
+ * \copyright GNU Public License
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	return 0;
+}
