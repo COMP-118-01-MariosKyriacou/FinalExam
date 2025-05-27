24d2d4ce96449eb8694e9dd49d0b5c90656db442
Marios Kyriacou <kyriacou.m6@live.unic.ac.cy>
2025-05-27 08:36:54 +0300

    File comment block and main function comment block

--- FinalExam.cpp
+++ FinalExam.cpp
@@ -1,7 +1,7 @@
 /**
  * \file FinalExam.cpp
- * \brief
- * \details
+ * \brief Matrix Manipulation
+ * \details This program is designed to prompt a menu to the user in order to manipulate a matrix (given by the user). We prompt a menu in order to select which operation they want to perfom (enter data, sum data, exit program) and then we perfom the operation.
  * \author Marios Kyriacou (U244N0037)
  * \version 0.1
  * \date 2025
@@ -12,6 +12,11 @@
 
 using namespace std;
 
+/**
+ * Main function: It is the main function in order to prompt the menu to the user and perform the operations selected by the user
+ * until they select to exit the program.
+ * @return Returns 0 to indicate successful execution
+ */
 int main() {
 	return 0;
 }
