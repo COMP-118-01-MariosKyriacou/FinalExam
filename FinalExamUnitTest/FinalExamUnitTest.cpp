c600a781fccceb30fb65d1dd4212bae4dc9cedb7
Marios Kyriacou <kyriacou.m6@live.unic.ac.cy>
2025-05-27 09:23:05 +0300

    Creation of unit test files

---
+++ FinalExamUnitTest/FinalExamUnitTest.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../FinalExam.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FinalExamUnitTest {
+	TEST_CLASS(FinalExamUnitTest) {
+		public:
+			TEST_METHOD(TestMethod1) {
+				// Arrange
+
+				// Act
+
+				// Assert
+			}
+	};
+}
